ecddbf2f4a2bf6337ce76e34a64d0cfc6e7e040c
mbarzegary <mbarzegary@msn.com>
2021-11-05 19:36:13 +0100

    read asli version and display it in about dialog

--- src/about.cpp
+++ src/about.cpp
@@ -1,5 +1,7 @@
+#include "global.h"
 #include "about.h"
 #include "ui_about.h"
+#include <QProcess>
 
 About::About(QWidget *parent) :
     QDialog(parent),
@@ -12,6 +14,23 @@ About::About(QWidget *parent) :
     ui->buildTimeLabel->setText(QString("Built on %1 %2").arg(buildDate).arg(buildTime));
 
     ui->qtVersionLabel->setText(QString("Based on Qt %1").arg(QT_VERSION_STR));
+
+    ui->asliVersionLabel->setText("Error reading ASLI version");
+    QProcess process;
+    QString dir = WORKING_DIR;
+    QString program = COMMAND_TO_RUN;
+    program = dir + program;
+    process.setProgram(program);
+    process.setArguments(QStringList() << "-version");
+    process.setWorkingDirectory(dir);
+    process.start();
+    process.waitForFinished();
+    QString output(process.readAllStandardOutput());
+    for (QString line : output.split("\n"))
+        if (!line.isEmpty())
+            if (line.toLower().startsWith("asli v"))
+                ui->asliVersionLabel->setText(line);
+
 
 //    this->setFixedSize(this->size());
     layout()->setSizeConstraint(QLayout::SetFixedSize);
