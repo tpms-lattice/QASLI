a3958c8f5a824ea12500861fa5bda00d79fb7fe7
mbarzegary <mbarzegary@msn.com>
2021-11-08 14:44:24 +0100

    change asli exec path and remove scripts

--- src/global.h
+++ src/global.h
@@ -4,16 +4,16 @@
 #include <QString>
 
 #ifdef Q_OS_LINUX
-#define COMMAND_TO_RUN "scripts/run.sh";
+#define COMMAND_TO_RUN "ASLI";
 #elif defined(Q_OS_WINDOWS)
 //QString commandToRun = "wsl \"scripts/run.sh\"";
 #define COMMAND_TO_RUN "ASLI.exe";
 #endif
 
 #ifdef Q_OS_LINUX
-#define WORKING_DIR "scripts/run.sh";
+#define WORKING_DIR "./";
 #elif defined(Q_OS_WINDOWS)
-#define WORKING_DIR "bin\\";
+#define WORKING_DIR ".\\";
 #endif
 
 #endif // GLOBAL_H
