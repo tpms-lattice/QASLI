6974363ffc226c1008dc6c1760baeff50767ff68
mbarzegary <mbarzegary@msn.com>
2022-06-22 01:23:29 +0200

    fix rhi renderer issue in qt 6

--- src/main.cpp
+++ src/main.cpp
@@ -1,9 +1,15 @@
 #include <QApplication>
 #include "mainwindow.h"
+#include <Qt3DRender/qt3drender-config.h>
 
 int main(int argc, char *argv[])
 {
     QApplication a(argc, argv);
+
+    #if QT_VERSION >= QT_VERSION_CHECK(6, 0, 0)
+        qputenv("QT3D_RENDERER", "opengl");
+    #endif
+
     MainWindow w;
     w.show();
     
